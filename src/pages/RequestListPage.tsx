ea90836d9795d3fe44196bd5d6a9c4dc81b112ca
taalnorbert <taalnorbert@gmail.com>
2026-01-06 20:40:50 +0100

    REARRANGE - remove unused HomePage and RequestListPage components; update router configuration

--- src/pages/RequestListPage.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react";
-
-const RequestListPage: React.FC = () => {
-    return (
-        <div>asdasd222</div>
-    )
-}
-
-export default RequestListPage;
--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,29 +1,16 @@
 
-import HomePage from '@/pages/HomePage';
 import RequestCreatePage from '@/pages/RequestCreatePage';
-import RequestListPage from '@/pages/RequestListPage';
 import { type RouterConfig } from '@/utils/definitions';
 
 
 export const router: RouterConfig = {
     elements: [
-        { 
+        
+        {
             mode: "PROTECTED",
             path: "/",
-            component: <HomePage/>,
-            navbar: true,
-        },
-        {
-            mode: "PROTECTED",
-            path: "/requests/new",
             component: <RequestCreatePage/>,
-            navbar: true,
-        },
-        {
-            mode: "PROTECTED",
-            path: "/requests/list",
-            component: <RequestListPage/>,
-            navbar: true
+            navbar: false,
         }
     ],
 
