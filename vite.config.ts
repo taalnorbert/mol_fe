325e542848178c32375d5501e8f4be569c972d1f
Ragasits Ádám <ragasits.adam@sze.hu>
2026-01-06 22:04:31 +0100

    ci update3

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
+  server: {
     port: 80,
     allowedHosts: ["torzsdb.ddc-int.sze.hu"]
   },
+  base: '/mol_fe/',
   resolve: {
     alias: {
       '@': '/src'
